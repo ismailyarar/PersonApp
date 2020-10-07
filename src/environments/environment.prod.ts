f21d31425ddf0ec42515c9e61fbc9d3655918190
ismail yarar <ismyarar@gmail.com>
2020-10-07 17:29:02 +0300

    master

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDXX2eAzrqcnjA588fG56MwlccEetH6IVM',
-    authDomain: 'personapp-7a750.firebaseapp.com',
-    databaseURL: 'https://personapp-7a750.firebaseio.com',
-    projectId: 'personapp-7a750',
-    storageBucket: 'personapp-7a750.appspot.com',
-    messagingSenderId: '293373103427',
-    appId: '1:293373103427:web:f049ee276ac5df85e833f6',
-    measurementId: 'G-GK9S4FMP47',
+    apiKey: "AIzaSyDXrqcnjA588fG56MwlccEetH6IVM",
+  authDomain: "persopp-7a750.ebaseapp.com",
+  databaseURL: "https://perapp-7a750.firebaseio.com",
+  projectId: "persapp-7a750",
+  storageBucket: "persopp-7a750.appspot.com",
+  messagingSenderId: "2933103427",
+  appId: "1:2933703427:web49ee276ac5df85e833f6",
+  measurementId: "G-GK9S4F47"
   },
 };
